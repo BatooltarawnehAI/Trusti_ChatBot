04a1cc9e98603b142139fd841150ec5e6e266650
BatooltarawnehAI <batoolatarawneh@gmail.com>
2025-02-03 03:47:54 -0500

    set up my local package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Trusti Project', #project name so we can create this package
+    version= '0.0.0',
+    author= 'Batool Gizem Derick',
+    author_email= 'batoolatarawneh@gmail.com',
+    packages= find_packages(),#to consider init as my local package
+    install_requires = []
+
+)
